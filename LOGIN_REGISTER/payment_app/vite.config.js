5df2cab7b4738203a76d1da586d25f4add7183d3
colby <mastercolby699@gmail.com>
2024-10-08 14:42:50 +0200

    commit login and register design

--- LOGIN_REGISTER/payment_app/vite.config.js
+++ LOGIN_REGISTER/payment_app/vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import fs from 'fs';
 import path from 'path';
-import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url';
+import react from '@vitejs/plugin-react';
+
+// Simulate __dirname in ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     https: {
-      key: fs.readFileSync(path.resolve(__dirname, '../keys/privatekey.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, '../keys/certificate.pem')),
+      // Update the path to point to the server/keys folder
+      key: fs.readFileSync(path.resolve(__dirname, '../server/keys/privatekey.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, '../server/keys/certificate.pem')),
     },
   },
   plugins: [react()],
-})
+});
